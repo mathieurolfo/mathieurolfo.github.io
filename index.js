ed9f5f1af39d90fb428445f76094637fd132ac00
Mathieu Rolfo <rolfom01@gmail.com>
2017-02-11 10:48:28 -0500

    removed gif from main page to improve loading times

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-window.onload = function() {
-	document.getElementById('brainimg').style.display = 'none';
-	document.getElementById('brain').style.visibility = 'visible';
-};
+// window.onload = function() {
+// 	document.getElementById('brainimg').style.display = 'none';
+// 	document.getElementById('brain').style.visibility = 'visible';
+// };
 
